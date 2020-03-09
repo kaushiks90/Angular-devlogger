c0fa99fa3c3301a8bce454dd0a47c8293237fdfc
kaushiks90 <kaushiks90@gmail.com>
2020-03-09 21:50:17 +0530

    added local storage

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -38,13 +38,23 @@ export class LogService {
     this.logs = [];
   }
   getLogs(): Observable<Log[]> {
-    return of(this.logs);
+    if (localStorage.getItem("logs") == null) {
+      this.logs = [];
+    } else {
+      this.logs = JSON.parse(localStorage.getItem("logs"));
+    }
+    return of(
+      this.logs.sort((a, b) => {
+        return b.date == a.date;
+      })
+    );
   }
   setFormLog(log: Log) {
     this.logSource.next(log);
   }
   addLog(log: Log) {
     this.logs.unshift(log);
+    localStorage.setItem("logs", JSON.stringify(this.logs));
   }
   updLog(log: Log) {
     this.logs.forEach((curr, index) => {
@@ -53,6 +63,7 @@ export class LogService {
       }
     });
     this.logs.unshift(log);
+    localStorage.setItem("logs", JSON.stringify(this.logs));
   }
   deleteLog(log: Log) {
     this.logs.forEach((curr, index) => {
@@ -60,6 +71,7 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+    localStorage.setItem("logs", JSON.stringify(this.logs));
   }
   clearState() {
     this.stateSource.next(true);
