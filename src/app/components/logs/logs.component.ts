8956a938044e182c002415653a4f3e81a11e22b8
kaushiks90 <kaushiks90@gmail.com>
2020-03-09 09:59:26 +0530

    Removed hardcoded data from html and brought it from ts

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-logs',
-  templateUrl: './logs.component.html',
-  styleUrls: ['./logs.component.css']
+  selector: "app-logs",
+  templateUrl: "./logs.component.html",
+  styleUrls: ["./logs.component.css"]
 })
 export class LogsComponent implements OnInit {
-
-  constructor() { }
+  logs: {
+    id: string;
+    text: string;
+    date: any;
+  }[];
+  constructor() {}
 
   ngOnInit() {
+    this.logs = [
+      {
+        id: "1",
+        text: "Generated Components",
+        date: new Date("12/26/2017 12:54:23")
+      },
+      {
+        id: "2",
+        text: "Added Components",
+        date: new Date("12/27/2017 12:54:23")
+      },
+      {
+        id: "3",
+        text: "Updated Components",
+        date: new Date("12/28/2017 12:54:23")
+      }
+    ];
   }
-
 }
